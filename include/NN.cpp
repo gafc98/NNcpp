606c7518576ed60964bc918be9b0135501f25a48
gafc98 <gafc@live.com.pt>
2023-04-08 22:01:44 +0200

    finally

--- include/NN.cpp
+++ include/NN.cpp
@@ -12,11 +12,16 @@ using std::string;
 
 struct Layer
 {
-  Vector activations;
-  Vector nets;
-  string non_linearity_type;
+  Vector a; // actiovations
+  Vector z; // net
   Matrix W;
-  Vector deltas;
+  Vector b;
+
+  Vector jac_z;
+  Matrix jac_W;
+  Vector jac_b;
+
+  string non_linearity_type;
 };
 
 float sigmoid(const float z)
@@ -40,10 +45,12 @@ public:
 
   void generate_layers()
   {
+    Layer l;
+    _layers.push_back(l); // this will be used for 1st input
     for (size_t i = 1; i < _ns.size(); i++)
     {
-      Layer l; 
-      l.W = _layer_init_multiplier * Matrix::Random(_ns[i], _ns[i-1] + 1); // + 1 due to bias
+      l.W = _layer_init_multiplier * Matrix::Random(_ns[i], _ns[i-1]);
+      l.b = _layer_init_multiplier * Vector::Random(_ns[i]);
       _layers.push_back(l);
     }
   };
@@ -59,45 +66,60 @@ public:
   {
     std::cout << "activations:\n";
     for (auto & l : _layers)
-      std::cout << l.activations << "\n\n";
+      std::cout << l.a << "\n\n";
   };
 
-  void print_layers_deltas()
+
+  void print_layers_jacobian()
   {
-    std::cout << "deltas:\n";
+    size_t i = 0;
     for (auto & l : _layers)
-      std::cout << l.deltas << "\n\n";
-  };
+    {
+      std::cout << "layer: " << i++ << "\n";
+      std::cout << "jac_z:\n" << l.jac_z << "\n\n";
+      std::cout << "jac_b:\n" << l.jac_b << "\n";
+      std::cout << "jac_W:\n" << l.jac_W << "\n";
+    }
+  }
 
   void feed_forward(Vector x)
   {
-    _append_one_to_vec(x);
-
-    // first layer activations
-    _layers[0].nets = _layers[0].W * x;
-    _layers[0].activations = _func_of_vec(_layers[0].nets, &sigmoid);
-    _append_one_to_vec(_layers[0].activations);
-
+    _layers[0].a = x; // set first layer activations to input
     for (size_t i = 1; i < _layers.size(); i++)
     {
-      _layers[i].nets = _layers[i].W * _layers[i-1].activations;
-      _layers[i].activations = _func_of_vec(_layers[i].nets, &sigmoid);
-      _append_one_to_vec(_layers[i].activations);
+      _layers[i].z = _layers[i].W * _layers[i - 1].a + _layers[i].b;
+      _layers[i].a = _func_of_vec(_layers[i].z, &sigmoid);
+    } 
+  };
+
+  Vector get_loss(Vector y)
+  {
+    Vector dist = _layers[_layers.size() - 1].a - y;
+    return dist.cwiseProduct(dist);
+  };
+
+  void layer_jacobian(Vector x, Vector y)
+  {
+    size_t size_layers = _layers.size();
+
+    _layers[size_layers - 1].jac_z = _layers[size_layers - 1].a - y; // derivative of loss
+    _layers[size_layers - 1].jac_b = _layers[size_layers - 1].jac_z;
+    _layers[size_layers - 1].jac_W = _layers[size_layers - 1].jac_z * (_layers[size_layers - 2].a.transpose());
+
+    for (size_t i = size_layers - 1; i > 1; i--)
+    {
+      _layers[i - 1].jac_z = ( _layers[i].W.transpose() * _layers[i].jac_z ).cwiseProduct(_func_of_vec(_layers[i-1].a, &sigmoid_derivative));
+      _layers[i - 1].jac_b = _layers[i - 1].jac_z;
+      _layers[i - 1].jac_W = _layers[i - 1].jac_z * (_layers[i - 2].a.transpose());
     }
   };
 
-  void layer_jacobian()
+  void update_net()
   {
-    // last layer
-    _layers[_layers.size() - 1].deltas = _func_of_vec(_layers[_layers.size() - 1].activations, &sigmoid_derivative); //.segment(0, (_layers[_layers.size() - 1].activations).size() - 1)
-    //std::cout << "deltas:\n" << _layers[_layers.size() - 1].deltas << "\n\n";
-    for (size_t i = _layers.size() - 1; i > 0; i--)
+    for (auto & l : _layers)
     {
-      size_t size_deltas = _layers[i].deltas.size();
-      _layers[i - 1].deltas = (_layers[i].W).transpose() * _layers[i].deltas.segment(0, size_deltas - 1);
-      //std::cout << _func_of_vec(_layers[i].activations, &sigmoid_derivative);
-      _layers[i - 1].deltas = _layers[i - 1].deltas.cwiseProduct(_func_of_vec(_layers[i-1].activations, &sigmoid_derivative));
-      //std::cout << _layers[i - 1].deltas << "\n\n";
+      l.b += - _learning_rate * l.jac_b;
+      l.W += - _learning_rate * l.jac_W;
     }
   };
 
@@ -118,6 +140,7 @@ private:
   std::vector<uint16_t> _ns;
   std::vector<Layer> _layers;
   float _layer_init_multiplier = 1.0;
+  float _learning_rate = 0.001;
 };
 
 #endif // FEED_FORWARD_NET
--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <Eigen/Dense>
@@ -22,8 +23,155 @@ int main()
   std::cout << x;
   std::cout << "\n\n";
 
-  net.feed_forward(x);
-  net.print_layers_activations();
-  net.layer_jacobian();
-  net.print_layers_deltas();
+  std::cout << "\n\n";
+  Vector y(1);
+  y << 0.7;
+  std::cout << x;
+  std::cout << "\n\n";
+
+  for (size_t i = 0; i < 2000; i++)
+  {
+    net.feed_forward(x);
+    std::cout <<"loss: " << net.get_loss(y) << "\n";
+    //net.print_layers_activations();
+    net.layer_jacobian(x, y);
+    //net.print_layers_jacobian();
+    net.update_net();
+  }
+  
 }
+
+/*
+
+#include <iostream>
+#include <cmath>
+#include <eigen/dense>
+
+double Sigmoid(const double z) {
+    return 1.0 / (1.0 + exp(-z));
+}
+// Assuming z are already results of sigmoid function,
+// if not it should be return Sigmoid(z) * (1.0 - Sigmoid(z));
+double SigmoidDerivative(const double z) {
+    return z * (1.0 - z);
+}
+
+class NeuralNetwork {
+public:
+    NeuralNetwork(const Eigen::MatrixXd &x, const Eigen::VectorXd &y);
+    void Feedforward();
+    void BackPropagation();
+    Eigen::VectorXd predicted_output;
+private:
+    double GetLossValue() const;
+    uint32_t num_samples_;
+    uint32_t num_features_;
+    Eigen::MatrixXd input_;
+    Eigen::VectorXd real_output_;
+    // Assuming we have 2 layers
+    // Neuron count per layer
+    const uint32_t neuron_count_1_ = 4;
+    const uint32_t neuron_count_2_ = 1;
+    // Weight matrixes per layer
+    // Assuming biases are 0
+    Eigen::MatrixXd weights_1_;
+    Eigen::MatrixXd weights_2_;
+    // Temporary variable store output from first layer, changes after every feedforward
+    // size will be rows - number of input samples, cols - number of neurons in 1st layer
+    Eigen::MatrixXd output_1_;
+};
+
+NeuralNetwork::NeuralNetwork(const Eigen::MatrixXd & x, const Eigen::VectorXd & y) {
+    input_ = x;
+    real_output_ = y;
+    num_samples_ = (uint32_t)input_.rows();
+    num_features_ = (uint32_t)input_.cols();
+    assert(num_samples_ == real_output_.rows());
+    predicted_output = Eigen::VectorXd::Zero(num_samples_);
+    // Random seed
+    //srand((unsigned int)time(0));
+    weights_1_ = Eigen::MatrixXd::Random(num_features_, neuron_count_1_);
+    weights_2_ = Eigen::MatrixXd::Random(neuron_count_1_, neuron_count_2_);
+    std::cout << "layer 1 weights:\n " << weights_1_ << std::endl;
+    std::cout << "layer 2 weights:\n " << weights_2_ << std::endl;
+}
+
+// Move through layers to final prediction
+void NeuralNetwork::Feedforward() {
+    // Layer 1
+    // Take all samples at once and calculate dot product for all layer 1 neurons (cols)
+    // for each sample (rows)
+    Eigen::MatrixXd z_1 = input_ * weights_1_;
+    //std::cout << "layer 1 outuput numerical:\n " << z_1 << std::endl;
+    // Squishify values in interval 0-1 using Sigmoid function
+    output_1_ = z_1.unaryExpr(std::ref(Sigmoid));
+    //std::cout << "layer 1 outuput:\n " << output_1_ << std::endl;
+    // Layer 
+    // Calculate dot product to get a predicted vector where each row is for input sample
+    Eigen::VectorXd z_2 = output_1_ * weights_2_;
+    //std::cout << "layer 2 outuput sum:\n " << z_2 << std::endl;
+    // Squish using Sigmoid function
+    predicted_output = z_2.unaryExpr(std::ref(Sigmoid));
+    //std::cout << "layer 2 outuput:\n " << predicted_output << std::endl;
+}
+
+// Loss function - sum of squared errors: sum ( real-predicted )^2
+double NeuralNetwork::GetLossValue() const {
+    // get difference vector
+    Eigen::VectorXd loss_vector = real_output_ - predicted_output;
+    // Square every element
+    loss_vector = loss_vector.unaryExpr([](double x) { return x * x; });
+    // sum them all up
+    double loss_value = loss_vector.sum();
+    std::cout << "Loss value: " << loss_value << std::endl;
+    return loss_value;
+}
+
+// Go back and update weights in layers from last to first
+// Loss function - sum of squared errors: sum ( real-predicted )^2
+// Derivative of loss function = 2 * (real-predicted) * sigmoid_der * input
+void NeuralNetwork::BackPropagation() {
+     GetLossValue();
+    // Using derivative of loss function, find the amount to which reduce weights
+    // if we move to direction of loss minimum
+    // Layer 2 weight adjustment delta.
+    Eigen::VectorXd delta_output = real_output_ - predicted_output;
+    // Sigmoid derivative
+    Eigen::VectorXd predicted_output_der = predicted_output.unaryExpr(std::ref(SigmoidDerivative));
+    // Weight delt to adjust for each weight in layer 2
+    Eigen::VectorXd delta_w_2 = output_1_.transpose() * (2 * delta_output.cwiseProduct(predicted_output_der));
+    //std::cout << "layer 2 delta w:\n " << delta_w_2 << std::endl;
+    // Layer 1 weight adjustment delta.
+    // Calculate like in previous layer but uglier because there is function inside function that need to be calculated
+    Eigen::MatrixXd delta_w_1 = input_.transpose() * (2 * delta_output.cwiseProduct(predicted_output_der) * weights_2_.transpose()).cwiseProduct(output_1_.unaryExpr(std::ref(SigmoidDerivative)));
+    //std::cout << "layer 1 delta w:\n " << delta_w_1 << std::endl;
+    // Update
+    weights_1_ += delta_w_1;
+    weights_2_ += delta_w_2;
+}
+
+
+
+int main() {
+    const uint32_t num_samples = 4;
+    const uint32_t num_features = 3;
+    // Input samples
+    Eigen::MatrixXd input(num_samples, num_features);
+    input << 0.0, 0.0, 1.0,
+             0.0, 1.0, 1.0,
+             1.0, 0.0, 1.0,
+             1.0, 1.0, 1.0;
+    // Real outputs (column vector)
+    Eigen::VectorXd output(num_samples);
+    output << 0, 1, 1, 0;
+
+    NeuralNetwork net(input, output);
+    for (int i = 0; i < 15; i++) {
+        net.Feedforward();
+        net.BackPropagation();
+    }
+
+    std::cout << "Predicted output:\n " << net.predicted_output << std::endl;
+    return 0;
+}
+*/
